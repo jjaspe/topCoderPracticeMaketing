a8a3d91a81574a0ccb9334ba29f527ffadecf0a2
jjaspe <jjaspe09@hotmail.com>
2014-11-14 22:20:26 -0500

    Added string loader and tests

---
+++ topCoderPracticeMarketing/StringLoader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace topCoderPracticeMarketing
+{
+    public class StringLoader
+    {
+        public bool[,] loadDataAsBooleans(String[] data)
+        {
+            bool[,] grid=new bool[data.Length,data.Length];
+            for(int i=0;i<data.Length;i++)
+            {
+            }
+            return grid;
+        }
+
+    }
+}
